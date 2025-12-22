d55b9c79a1495fb8bed74a1a4e34d93ec696e135
M.K.Thiong'o <kamauthiongo58@gmail.com>
2025-12-22 20:19:46 +0300

    update challenge 1

--- superheroes/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = "supersecretkey"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/app.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
